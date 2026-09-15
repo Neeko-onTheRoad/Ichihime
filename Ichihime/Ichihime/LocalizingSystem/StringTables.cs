03238789411dee1c4039d8854d0bf0294ce1ca6d
wata548 <watashimokia@gmail.com>
2026-09-15 21:13:42 +0900

    comp error fix

--- Ichihime/Ichihime/LocalizingSystem/StringTables.cs
+++ Ichihime/Ichihime/LocalizingSystem/StringTables.cs
@@ -61,8 +61,6 @@ public class StringTables : IReadOnlyDictionary<string, StringTable> {
 
 	}
 
-	private void 
-
 	//======================================================================| Operators
 
 	public StringTable this[string key] => _tables[key];
